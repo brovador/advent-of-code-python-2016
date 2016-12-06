f3f3245fe5b07ec8b19bac45dfc7aaa0f141eda4
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2016-12-06 10:15:22 +0100

    Day 6 cleared 🎅🏾

---
+++ day06/main2.py
@@ -0,0 +1,18 @@
+#encoding: utf-8
+from collections import Counter
+
+def main():
+    input_file = './input2.txt'
+
+    with open(input_file) as f:
+        messages = f.read().split('\n')
+    
+    m_len = len(messages[0])
+    counts = [[] for i in range(m_len)]
+    for m in messages:
+        map(lambda i: counts[i].append(m[i]), range(m_len))
+    counts = [Counter(c) for c in counts]
+    print ''.join([c.most_common()[-1][0] for c in counts])
+
+if __name__ == '__main__':
+    main()

3e5065dabdace26bd7ab942db4e61772e0c42700
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2016-12-24 18:38:46 +0100

    Day 16 cleared 🎄

---
+++ day16/main.py
@@ -0,0 +1,37 @@
+#encoding: utf-8
+
+def main():
+    input_file = './input.txt'
+    disc_length = 272
+
+    with open(input_file) as f:
+        initial_state = f.read()
+    
+    def fill_data(a):
+        b = a + '0' + ''.join(['0' if x == '1' else '1' for x in a[::-1]])
+        if len(b) < disc_length:
+            b = fill_data(b)
+        return b[:disc_length]
+    
+    def generate_checksum(data):
+        s = ''.join(['1' if data[i] == data[i + 1] else '0' for i in range(0, len(data), 2)])
+        if len(s) % 2 == 0:
+            s = generate_checksum(s)
+        return s
+
+    
+    data = fill_data(initial_state)
+    checksum = generate_checksum(data)
+    print checksum
+
+
+
+            
+
+
+
+            
+
+
+if __name__ == '__main__':
+    main()
